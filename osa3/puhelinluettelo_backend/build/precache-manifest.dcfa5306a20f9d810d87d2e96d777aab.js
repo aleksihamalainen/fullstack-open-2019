18faa50f12c7fd95b56e076d7a875edfa8dd5a6d
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2019-06-16 14:29:11 +0300

    fixed submodule issue

---
+++ osa3/puhelinluettelo_backend/build/precache-manifest.dcfa5306a20f9d810d87d2e96d777aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a921ffaaf3d74214092bcb83ee920e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d32cdd3a19490876a42e",
+    "url": "/static/css/main.0f064136.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "d32cdd3a19490876a42e",
+    "url": "/static/js/main.71c4d524.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
