0da14a7f2564a95549805847bbe593d3eaa3cabb
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-01-03 19:21:24 +0200

    4.17 done

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,15 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  blogs = await Blog.find({})
+  blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response, next) => {
+  const users = await User.find({})
+  const user = users[0]
+  const body = { ...request.body, user: user._id }
+  const blog = new Blog(body)
   try {
-    const blog = new Blog(request.body)
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.json(savedBlog.toJSON())
   } catch (exception) {
     next(exception)
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.remove({})
@@ -90,6 +91,36 @@ test('fails when title or url is not defined', async () => {
         .send(withoutUrl)
         .expect(400)
 })
+
+describe('when there is initially one user at db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+      const user = new User({ username: 'root', password: 'sekret' })
+      await user.save()
+    })
+  
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+  
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen',
+      }
+  
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+  
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+  
+      const usernames = usersAtEnd.map(u => u.username)
+      expect(usernames).toContain(newUser.username)
+    })
+  })
 
 afterAll(() => {
     mongoose.connection.close()
