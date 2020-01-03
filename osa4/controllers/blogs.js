937d0c1ac572e43b1a567a4d2477d8b900091695
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-01-03 20:41:14 +0200

    osa4 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -8,11 +9,14 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const users = await User.find({})
-  const user = users[0]
-  const body = { ...request.body, user: user._id }
-  const blog = new Blog(body)
   try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const body = { ...request.body, user: user._id }
+    const blog = new Blog(body)
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
@@ -24,9 +28,18 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    const blog = Blog.findById(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+    if (blog.user.toString() === user.id.toString()) {
     await blog.remove()
     response.status(204).end()
+    } else {
+      response.status(403).json({ error: 'unauthorized' })
+    }
   } catch (exception) {
     next(exception)
   }
