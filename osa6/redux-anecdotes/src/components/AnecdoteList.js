66d4e49739fb4aeeee73113e255e20f03b822288
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-07-15 18:56:40 +0300

    6.12

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
+import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter)
   const dispatch = useDispatch();
 
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+  const filteredAnecdotes = filter === '' ? sortedAnecdotes : anecdotes.filter(a => a.content.toLowerCase().includes(filter))
 
   const vote = (id) => {
     const anecdote = anecdotes.find((a) => a.id === id);
     dispatch(voteAnecdote(anecdote));
+    dispatch(setNotification(`you voted ${anecdote.content}`))
+    setTimeout(() => {dispatch(setNotification(''))}, 5000)
   };
-  return sortedAnecdotes.map((anecdote) => (
+  return filteredAnecdotes.map((anecdote) => (
     <div key={anecdote.id}>
       <div>{anecdote.content}</div>
       <div>
