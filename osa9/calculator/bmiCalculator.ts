918a5ee9eb673a04b03fdfa99e105c0f8c911816
aleksihamalainen <aleksi.hamalainen5@gmail.com>
2020-07-30 17:41:15 +0300

    9.3

---
+++ osa9/calculator/bmiCalculator.ts
@@ -0,0 +1,36 @@
+interface Values {
+    height: number,
+    weight: number
+}
+
+const parseBMI = (args: Array<string>): Values => {
+    if (args.length !== 4) throw new Error("Invalid number of arguments")
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error("Provided values were not numbers")
+    }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi: number = weight / Math.pow(height / 100, 2)
+    if (bmi < 18.5) {
+        return "Underweight"
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return "Normal weight"
+    } else if (bmi >= 25 && bmi < 30) {
+        return "Overweight"
+    } else if (bmi >= 30) {
+        return "Obese"
+    }
+}
+
+try {
+    const {height, weight} = parseBMI(process.argv)
+    console.log(`You are: ${calculateBmi(height, weight)}`)
+} catch (e) {
+    console.log(e.message)
+}
---
+++ osa9/calculator/exerciseCalculator.ts
@@ -0,0 +1,59 @@
+type ratingNumber = 1 | 2 | 3
+
+interface exerciseObject {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: ratingNumber,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+interface exerciseValues {
+    exerciseHours: Array<number>,
+    target: number
+}
+
+const parseExercise = (args: Array<string>): exerciseValues => {
+    const filteredArgs = args.slice(2, 2).concat(args.slice(2, args.length))
+    if (filteredArgs.map(a => isNaN(Number(a))).includes(true)) throw new Error("Provided values were not numbers")
+    if (filteredArgs.length < 2) throw new Error("Not enough arguments")
+    return {
+        exerciseHours: filteredArgs.slice(1).map(a => Number(a)),
+        target: Number(filteredArgs[0])
+    }
+}
+
+const calculateExercises = (exerciseHours: Array<number>, target: number): exerciseObject => {
+    const periodLength: number = exerciseHours.length
+    const trainingDays: number = exerciseHours.filter(h => h !== 0).length
+    const sum: number = exerciseHours.reduce((a, b) => a + b, 0)
+    const average: number = (sum / periodLength) || 0
+    const success: boolean = average >= target ? true : false
+    const ratingDescription: string = "not too bad but could do better"
+    let rating: ratingNumber = 1
+    if (trainingDays === periodLength && success) {
+        rating = 3
+    } else if (success) {
+        rating = 2
+    } else {
+        rating = 1
+    }
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+try {
+    const {exerciseHours, target} = parseExercise(process.argv)
+    console.log(calculateExercises(exerciseHours, target))
+} catch (e) {
+    console.log(e.message)
+}
